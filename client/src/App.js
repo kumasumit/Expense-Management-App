eee05d855eb4775231b7cc6743962d4648c60579
Kumar Sumit <kumarsumit1989@gmail.com>
2022-10-30 06:30:50 +0530

    Add react to the current project

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <h1 className="text-center">Hello</h1>
+      
+    </div>
+  );
+}
+
+export default App;
