4c3e2e900d7b7bf2aae0c498e0136fdd622b95ba
Kumar Sumit <kumarsumit1989@gmail.com>
2022-10-30 07:16:32 +0530

    Modify usersController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -18,20 +18,26 @@ const loginUser = async (req, res) => {
     //if user is found corresponding to that email address
     //compare passwords entered by user and the password saved in the database
     if (user.password !== password) {
-      return res.status(401).json({ msg: "Password does not match" });
+      return res.status(401).json({
+        success: false,
+        msg: "Password does not match" });
       //401 is Unauthorized response code
     }
 
     // Case 3: if user is found corresponding to that email address,and passwords match
-
     res.status(200).json({
+      success:true,
       msg: "User logged in successfully",
       user,
     });
     //200 is Successful response code, here we return the user object
   } catch (error) {
-    return res.status(400).json({ msg: error });
-    // 400 is bad client request
+    return res.status(500).json({
+      success: false, 
+      msg: error 
+    });
+    // 500 is internal server error code
+
   }
 };
 //register-SignUp Action-Callback
@@ -40,20 +46,25 @@ const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
     const user = await usersModal.findOne({ email });
-    // Case 1: If no user is found for entered email address
-    //means the email is not registered and is unique
-    if (!user) {
-        const newUser = new usersModal({ name, email, password });
-        await newUser.save();
-        res.status(201).json({ msg: "User created successfully", newUser });
-        //201 is Created success status response code
-        //here we return the new created user object
+    // Case 1: If user is found for entered email address
+    //means the email is already registered and is not unique
+    if (user) {
+      return res.status(400).json({ success: false, msg: "Email already exists" });
+      //400 is bad client request
     }
-    
-  } catch (error) {
-    return res.status(400).json({ msg: error });
-    // 400 is bad client request
-  }
+
+    // Case 2: If user is not found for entered email address
+    //means the email is not already registered and is unique
+    const newUser = new usersModal({ name, email, password });
+    await newUser.save();
+    res.status(201).json({ msg: "User created successfully", newUser });
+    //201 is Created success status response code
+    //here we return the new created user object
+  }catch (error) {
+  return res.status(500).json({ success: false,msg: error });
+  // 500 is server error
+
+}
 };
 
 module.exports = { loginUser, registerUser };
